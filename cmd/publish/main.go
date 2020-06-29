f9f83dd5fe804cde89c3a7819ceb538954a19b08
Sumanth Chinthagunta <xmlking@gmail.com>
2020-06-29 10:59:46 -0700

    Update main.go

--- cmd/publish/main.go
+++ cmd/publish/main.go
@@ -1,36 +1,26 @@
 package main
 
 import (
-    "context"
-
-    "cloud.google.com/go/pubsub"
     "github.com/rs/zerolog/log"
+    "github.com/xmlking/grpc-starter-kit/shared/broker"
+    "github.com/xmlking/grpc-starter-kit/shared/config"
 )
 
-
+var (
+    cfg = config.GetConfig()
+)
 
 func main() {
     // os.Setenv("PUBSUB_EMULATOR_HOST", "http://localhost:8085")
     // os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "~/my-json.json")
 
-    ctx := context.Background()
-    client, err := pubsub.NewClient(ctx, "micro-starter-kit")
-    if err != nil {
+    broker.DefaultBroker = broker.NewBroker(broker.ProjectID("my-project-id"));
+
+    header := map[string]string{"sumo": "demo"}
+    body   := []byte("ABC€")
+    msg := broker.Message{Header: header, Body: body}
+
+    if err := broker.Publish("ingestion-in-dev", &msg); err != nil {
         log.Error().Err(err).Send()
     }
-
-    t := client.Topic("streaming-input")
-
-    result := t.Publish(ctx, &pubsub.Message{
-        Data: []byte("ABC€"),
-    })
-    // Block until the result is returned and a server-generated
-    // ID is returned for the published message.
-    id, err := result.Get(ctx)
-    if err != nil {
-        log.Error().Err(err).Send()
-    }
-    log.Info().Msgf("Published a message; msg ID: %v\n", id)
 }
-
-
