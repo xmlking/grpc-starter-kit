7a9c19ebb71750faeaf61734545bc8326c8a4312
Sumanth Chinthagunta <xmlking@gmail.com>
2020-06-29 11:00:37 -0700

    Update main.go

--- cmd/subscribe/main.go
+++ cmd/subscribe/main.go
@@ -1,27 +1,30 @@
 package main
 
 import (
-    "context"
-
-    "cloud.google.com/go/pubsub"
     "github.com/rs/zerolog/log"
+    "github.com/xmlking/grpc-starter-kit/shared/broker"
+    "github.com/xmlking/grpc-starter-kit/shared/config"
 )
 
+var (
+    cfg = config.GetConfig()
+)
 
 func main() {
-    //os.Setenv("PUBSUB_EMULATOR_HOST", "http://localhost:8085")
+    // os.Setenv("PUBSUB_EMULATOR_HOST", "http://localhost:8085")
     // os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "~/my-json.json")
 
-    ctx := context.Background()
-    c, err := pubsub.NewClient(ctx, "micro-starter-kit") // Go autodetects the PUBSUB_EMULATOR_HOST variable
+    broker.DefaultBroker = broker.NewBroker(broker.ProjectID("my-project-id"));
+
+    myHandler := func(e broker.Event) error {
+        log.Info().Interface("event", e).Send()
+        return  nil
+    }
+
+    sub, err := broker.Subscribe("ingestion-in-dev", myHandler, broker.Queue("ingestion-in-dev"));
     if err != nil {
         log.Error().Err(err).Send()
     }
-
-    sub := c.Subscription("streaming-input")
-
-    err = sub.Receive(ctx, func(cctx context.Context, msg *pubsub.Message) {
-     println(msg)
-    })
-    log.Error().Err(err).Send()
+    sub.Unsubscribe()
 }
+
