d0564b9c351eac1f96aee99ffe2dcf88260b2f69
Sumanth Chinthagunta <xmlking@gmail.com>
2020-06-29 09:56:03 -0700

    Add files via upload

---
+++ shared/README.md
@@ -0,0 +1,7 @@
+# Broker
+
+
+## Reference 
+- https://github.com/nytimes/gizmo/blob/master/pubsub/gcp/gcp.go
+- https://github.com/lileio/pubsub/blob/master/providers/google/google.go
+- https://github.com/micro/go-plugins/tree/master/broker/googlepubsub
---
+++ shared/options.go
@@ -0,0 +1,193 @@
+package broker
+
+import (
+    "context"
+    "google.golang.org/api/option"
+    "time"
+)
+
+// https://github.com/cloudevents/sdk-go/blob/master/protocol/pubsub/v2/options.go
+
+const (
+    DefaultProjectEnvKey      = "GOOGLE_CLOUD_PROJECT"
+    DefaultTopicEnvKey        = "PUBSUB_TOPIC"
+    DefaultSubscriptionEnvKey = "PUBSUB_SUBSCRIPTION"
+)
+
+const (
+    DefaultAckDeadline       = 30 * time.Second
+    DefaultRetentionDuration = 25 * time.Hour
+)
+
+type Options struct {
+    Addrs []string
+
+    // Handler executed when error happens in broker mesage
+    // processing
+    ErrorHandler Handler
+
+    // Other options for implementations of the interface
+    // can be stored in a context
+    Context context.Context
+}
+
+type PublishOptions struct {
+    // Other options for implementations of the interface
+    // can be stored in a context
+    Context context.Context
+}
+
+type SubscribeOptions struct {
+    // AutoAck defaults to true. When a handler returns
+    // with a nil error the message is acked.
+    AutoAck bool
+    // Subscribers with the same queue name
+    // will create a shared subscription where each
+    // receives a subset of messages.
+    Queue string
+
+    // Other options for implementations of the interface
+    // can be stored in a context
+    Context context.Context
+}
+
+type Option func(*Options)
+
+type PublishOption func(*PublishOptions)
+
+// PublishContext set context
+func PublishContext(ctx context.Context) PublishOption {
+    return func(o *PublishOptions) {
+        o.Context = ctx
+    }
+}
+
+type SubscribeOption func(*SubscribeOptions)
+
+func NewSubscribeOptions(opts ...SubscribeOption) SubscribeOptions {
+    opt := SubscribeOptions{
+        AutoAck: true,
+    }
+
+    for _, o := range opts {
+        o(&opt)
+    }
+
+    return opt
+}
+
+// Addrs sets the host addresses to be used by the broker
+func Addrs(addrs ...string) Option {
+    return func(o *Options) {
+        o.Addrs = addrs
+    }
+}
+
+// DisableAutoAck will disable auto acking of messages
+// after they have been handled.
+func DisableAutoAck() SubscribeOption {
+    return func(o *SubscribeOptions) {
+        o.AutoAck = false
+    }
+}
+
+// ErrorHandler will catch all broker errors that cant be handled
+// in normal way, for example Codec errors
+func ErrorHandler(h Handler) Option {
+    return func(o *Options) {
+        o.ErrorHandler = h
+    }
+}
+
+// Queue sets the name of the queue to share messages on
+func Queue(name string) SubscribeOption {
+    return func(o *SubscribeOptions) {
+        o.Queue = name
+    }
+}
+
+// SubscribeContext set context
+func SubscribeContext(ctx context.Context) SubscribeOption {
+    return func(o *SubscribeOptions) {
+        o.Context = ctx
+    }
+}
+
+type clientOptionKey struct{}
+
+type projectIDKey struct{}
+
+type maxOutstandingMessagesKey struct{}
+
+type maxExtensionKey struct{}
+
+type createSubscription struct{}
+
+type deleteSubscription struct{}
+
+// ClientOption is a broker Option which allows google pubsub client options to be
+// set for the client
+func ClientOption(c ...option.ClientOption) Option {
+    return func(o *Options) {
+        if o.Context == nil {
+            o.Context = context.Background()
+        }
+        o.Context = context.WithValue(o.Context, clientOptionKey{}, c)
+    }
+}
+
+// ProjectID provides an option which sets the google project id
+func ProjectID(id string) Option {
+    return func(o *Options) {
+        if o.Context == nil {
+            o.Context = context.Background()
+        }
+        o.Context = context.WithValue(o.Context, projectIDKey{}, id)
+    }
+}
+
+// CreateSubscription prevents the creation of the subscription if it not exists
+func CreateSubscription(b bool) Option {
+    return func(o *Options) {
+        if o.Context == nil {
+            o.Context = context.Background()
+        }
+
+        o.Context = context.WithValue(o.Context, createSubscription{}, b)
+    }
+}
+
+// DeleteSubscription prevents the deletion of the subscription if it not exists
+func DeleteSubscription(b bool) Option {
+    return func(o *Options) {
+        if o.Context == nil {
+            o.Context = context.Background()
+        }
+
+        o.Context = context.WithValue(o.Context, deleteSubscription{}, b)
+    }
+}
+
+// MaxOutstandingMessages sets the maximum number of unprocessed messages
+// (unacknowledged but not yet expired) to receive.
+func MaxOutstandingMessages(max int) SubscribeOption {
+    return func(o *SubscribeOptions) {
+        if o.Context == nil {
+            o.Context = context.Background()
+        }
+
+        o.Context = context.WithValue(o.Context, maxOutstandingMessagesKey{}, max)
+    }
+}
+
+// MaxExtension is the maximum period for which the Subscription should
+// automatically extend the ack deadline for each message.
+func MaxExtension(d time.Duration) SubscribeOption {
+    return func(o *SubscribeOptions) {
+        if o.Context == nil {
+            o.Context = context.Background()
+        }
+
+        o.Context = context.WithValue(o.Context, maxExtensionKey{}, d)
+    }
+}
