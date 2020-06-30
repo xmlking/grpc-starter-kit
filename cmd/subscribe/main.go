636d70f6a9e1199710559dab39a9bf131d25b64e
Sumanth Chinthagunta <xmlking@gmail.com>
2020-06-29 19:44:49 -0700

    fix(broker): shutdown

--- cmd/subscribe/main.go
+++ cmd/subscribe/main.go
@@ -1,29 +1,55 @@
 package main
 
 import (
-    "github.com/rs/zerolog/log"
+	"os"
+	"os/signal"
 
-    "github.com/xmlking/grpc-starter-kit/shared/broker"
-    "github.com/xmlking/grpc-starter-kit/shared/config"
+	"github.com/rs/zerolog/log"
+
+	"github.com/xmlking/grpc-starter-kit/shared/broker"
+	"github.com/xmlking/grpc-starter-kit/shared/config"
 )
 
 var (
-    cfg = config.GetConfig()
+	cfg = config.GetConfig()
 )
 
 func main() {
-    broker.DefaultBroker = broker.NewBroker();
+	broker.DefaultBroker = broker.NewBroker()
 
-    myHandler := func(e broker.Event) error {
-        log.Info().Interface("event", e).Send()
-        e.Ack()
-        return  nil
-    }
+	myHandler := func(e broker.Event) error {
+		msg := e.Message()
+		log.Info().Interface("event.Message.Body", msg.Body).Send()
+		log.Info().Interface("event.Message.Header", msg.Header).Send()
+		log.Info().Interface("event.Message", msg).Send()
+		log.Info().Interface("event.Topic", e.Topic()).Send()
+		_ = e.Ack()
+		return nil
+	}
 
-    sub, err := broker.Subscribe("ingestion-in-dev", myHandler, broker.Queue("ingestion-in-dev"));
-    if err != nil {
-        log.Error().Err(err).Send()
-    }
-    sub.Unsubscribe()
+	var subs []broker.Subscriber
+
+	// like adding micro.NewSubscribe(...)
+	sub, err := broker.Subscribe("ingestion-in-dev", myHandler, broker.Queue("ingestion-in-dev"))
+	if err != nil {
+		log.Error().Err(err).Msg("Subscribe to `ingestion-in-dev` Topic failed")
+	}
+	subs = append(subs, sub)
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	<-ch
+	log.Info().Msg("Got to Go...")
+	//_ = sub.Unsubscribe()
+	//_ = broker.Disconnect()
+
+	// close all subs and then connection.
+	for _, sub := range subs {
+		log.Info().Msgf("Unsubscribing from topic: %s", sub.Topic())
+		sub.Unsubscribe()
+	}
+
+	if err := broker.Disconnect(); err != nil {
+		log.Fatal().Err(err).Msg("Unexpected disconnect error")
+	}
 }
-
